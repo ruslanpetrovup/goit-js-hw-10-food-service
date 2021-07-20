1399c849081475639e0c38fae86cec677b057f7c
Ruslan Petrov <48686933+ruslanpetrov228@users.noreply.github.com>
2021-07-20 17:04:45 +0300

    added files

--- src/index.js
+++ src/index.js
@@ -1,1 +1,40 @@
-console.log('suka');
+import cards from './cards.hbs';
+import menu from './menu.json';
+
+const listMenuEl = document.querySelector('.js-menu');
+const btnThem = document.querySelector('#theme-switch-toggle');
+const bodyEl = document.querySelector('body');
+
+
+const Theme = {
+  LIGHT: 'light-theme',
+  DARK: 'dark-theme',
+};
+    
+if (localStorage.getItem('Them') === Theme.DARK) {
+    bodyEl.classList.add(Theme.DARK);
+} else {
+    bodyEl.classList.add(Theme.LIGHT);
+}
+if (bodyEl.classList.contains(Theme.DARK)) {
+    btnThem.setAttribute('checked', true)
+} else {
+    btnThem.removeAttribute('checked');
+}
+menu.forEach(element => {
+    listMenuEl.insertAdjacentHTML('beforeend',cards(element));
+});
+
+const themPage = () => {
+    if (bodyEl.classList.contains(Theme.DARK)) {
+        bodyEl.classList.remove(Theme.DARK)
+        bodyEl.classList.add(Theme.LIGHT);
+        localStorage.setItem('Them', Theme.LIGHT)
+        return
+    }
+    bodyEl.classList.remove(Theme.LIGHT);
+    bodyEl.classList.add(Theme.DARK);
+    localStorage.setItem('Them', Theme.DARK)
+}
+
+btnThem.addEventListener('change', themPage)
